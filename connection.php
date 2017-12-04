b75da855ad200b4dcfaf2fa2a36598f0407f43b1
Hu  Kevin <huk@student2.cs.appstate.edu>
2017-12-04 18:39:46 -0500

    successful connection to db

--- connection.php
+++ connection.php
@@ -1,7 +1,19 @@
 <?php 
-$mydb = @mysql_connect("student2.cs.appstate.edu/huk", "huk", "900502862"); 
+$mydb = @mysql_connect("localhost", "huk", "900502862"); 
 if( !$mydb) 
 { 
     echo( "Connection to database server failed <br>"); 
     exit( ); 
 }
+
+$servername = "localhost";
+$username = "huk";
+$password = "900502862";
+$dbname = "huk";
+
+// Create connection
+$mysqli_conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($mysqli_conn->connect_error) {
+    die("Connection failed: " . $mysqli_conn->connect_error);
+} 
